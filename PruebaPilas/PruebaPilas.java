0243803225e0974d051a90d428c8c988fe76d239
itsErik-13 <erik.p.f.2001@gmail.com>
2022-04-19 21:46:55 +0200

    Added AEDI pilas

--- PruebaPilas/PruebaPilas.java
+++ PruebaPilas/PruebaPilas.java
@@ -1,10 +1,11 @@
 package PruebaPilas;
 
+import pila.*;
 import java.util.Stack;
 
 public class PruebaPilas {
     public static void main(String[] args) {
-        
+        System.out.println(esCorrectoAEDI("[()]"));
     }
     public static String codificar (String mensaje){
         String toRet = "";
@@ -38,6 +39,40 @@ public class PruebaPilas {
         }
         return toRet;
     }
+
+    public static String codificarAEDI(String mensaje){
+        String toRet = "";
+        Pila<Character> aux = new EnlazadaPila<>();
+        Character examine;
+        for (int i = 0; i < mensaje.length(); i++) {
+            examine = mensaje.charAt(i);
+            switch(examine){
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                while(!aux.esVacio()){
+                    toRet += aux.pop();
+                }
+                toRet += examine;
+                break;
+                default:
+                aux.push(examine);
+                break;
+            }
+        }
+        while(!aux.esVacio()){
+            toRet+=aux.pop();
+        }
+        return toRet;
+    }
+
     public static <E> E desapilarElemento (Stack<E> p, int n) throws IllegalArgumentException{
         E toRet;
         if(n < 0||n >= p.size()) throw new IllegalArgumentException();
@@ -51,6 +86,21 @@ public class PruebaPilas {
         }
         return toRet;
     }
+
+    public static <E> E desapilarElementoAEDI (Pila<E> p, int n) throws IllegalArgumentException{
+        E toRet;
+        if(n < 0||n >= p.tamaño()) throw new IllegalArgumentException();
+        Pila<E> aux = new EnlazadaPila<>();
+        for (int i = 0; i < n ; i++) {
+            aux.push(p.pop());
+        }
+        toRet = p.pop();
+        while(!aux.esVacio()){
+            p.push(aux.pop());
+        }
+        return toRet;
+    }
+
     public static String convertir (int numero){
         String toRet = "";
         Stack<Integer> p = new Stack<>();
@@ -60,6 +110,20 @@ public class PruebaPilas {
         }
         p.add(numero);
         while(!p.isEmpty()){
+            toRet+=p.pop();
+        }
+        return toRet;
+    }
+
+    public static String convertirAEDI (int numero){
+        String toRet = "";
+        Pila<Integer> p = new EnlazadaPila<>();
+        while(numero/2 >= 1){
+            p.push(numero%2);
+            numero = numero/2;
+        }
+        p.push(numero);
+        while(!p.esVacio()){
             toRet+=p.pop();
         }
         return toRet;
@@ -82,5 +146,24 @@ public class PruebaPilas {
             }
         }
         return toRet && aux.isEmpty();
+    }
+
+    public static boolean esCorrectoAEDI(String exp){
+        boolean toRet = true;
+        final String OPENING_DICC = "[{(" ;
+        final String CLOSING_DICC = "]})";
+        Pila<Character> aux = new EnlazadaPila<>();
+        for (int i = 0; i < exp.length(); i++) {
+            for (int j = 0; j < OPENING_DICC.length(); j++) {
+                if(exp.charAt(i) == OPENING_DICC.charAt(j)){
+                    aux.push(OPENING_DICC.charAt(j));
+                }
+                if(exp.charAt(i) == CLOSING_DICC.charAt(j)){
+                    if(aux.esVacio()|| aux.pop()!=OPENING_DICC.charAt(j))
+                        toRet=false;
+                }
+            }
+        }
+        return toRet && aux.esVacio();
     }
 }
